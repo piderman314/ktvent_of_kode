e6efb552df4edcb5f3b8e85c55227e2aad832504
Mark Arts <piderman+github@gmail.com>
2024-12-28 15:04:10 +0100

    2015 Day 1 Part 2

--- src/main/kotlin/advent/_2015/Day1.kt
+++ src/main/kotlin/advent/_2015/Day1.kt
@@ -5,16 +5,18 @@ import java.time.LocalDate
 
 class Day1 : Day(LocalDate.of(2015, 12, 1)) {
     override fun part1(input: String): String {
-        return input.toCharArray().sumOf { ch ->
-            when (ch) {
-                '(' -> 1L
-                ')' -> -1L
-                else -> 0L
-            }
-        }.toString()
+        return input.toCharArray().sumOf(::braceToAddend).toString()
     }
 
     override fun part2(input: String): String {
-        TODO("Not yet implemented")
+        val fold = input.toCharArray()
+            .runningFoldIndexed(-1 to 0L) { index, acc, ch -> (index + 1) to (acc.second + braceToAddend(ch)) }
+        return fold.find { it.second == -1L }!!.first.toString()
+    }
+
+    private fun braceToAddend(c: Char) = when (c) {
+        '(' -> 1L
+        ')' -> -1L
+        else -> 0L
     }
 }
--- src/test/kotlin/advent/_2015/Day1Test.kt
+++ src/test/kotlin/advent/_2015/Day1Test.kt
@@ -1,9 +1,10 @@
 package advent._2015
 
 import advent.DayTest
+import advent.TestInput
 
 class Day1Test : DayTest<Day1>(::Day1) {
-    override fun part1TestInput(): Map<String, String> =
+    override fun part1TestInput(): TestInput =
         mapOf(
             "(())" to "0",
             "()()" to "0",
@@ -14,6 +15,12 @@ class Day1Test : DayTest<Day1>(::Day1) {
             "))(" to "-1",
             ")))" to "-3",
             ")())())" to "-3"
+        )
+
+    override fun part2TestInput(): TestInput =
+        mapOf(
+            ")" to "1",
+            "()())" to "5"
         )
 
 }
