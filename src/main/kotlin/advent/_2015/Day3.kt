8ebe4c34fb6311f23ef588dc8f874e15b6e61809
Mark Arts <piderman+github@gmail.com>
2024-12-28 18:15:09 +0100

    2015 Day 3

---
+++ src/main/kotlin/advent/_2015/Day3.kt
@@ -0,0 +1,32 @@
+package advent._2015
+
+import advent.Day
+import advent.util.Location
+import advent.util.move
+import advent.util.toDirection
+import java.time.LocalDate
+
+class Day3 : Day(LocalDate.of(2015, 12, 3)) {
+    override fun part1(input: String): String {
+        return input.toCharArray().map(Char::toDirection)
+            .runningFold(Location(0, 0)) { location, direction -> location move direction }
+            .toSet()
+            .size
+            .toString()
+    }
+
+    override fun part2(input: String): String {
+        return input.toCharArray().map(Char::toDirection)
+            .runningFoldIndexed(Pair(Location(0, 0), Location(0, 0))) { index, locations, direction ->
+                if (index % 2 == 0) {
+                    Pair(locations.first move direction, locations.second)
+                } else {
+                    Pair(locations.first, locations.second move direction)
+                }
+            }
+            .flatMap { it.toList() }
+            .toSet()
+            .size
+            .toString()
+    }
+}
---
+++ src/test/kotlin/advent/_2015/Day3Test.kt
@@ -0,0 +1,21 @@
+package advent._2015
+
+import advent.DayTest
+import advent.TestInput
+
+class Day3Test : DayTest<Day3>(::Day3) {
+    override fun part1TestInput(): TestInput =
+        mapOf(
+            ">" to "2",
+            "^>v<" to "4",
+            "^v^v^v^v^v" to "2"
+        )
+
+    override fun part2TestInput(): TestInput =
+        mapOf(
+            "^v" to "3",
+            "^>v<" to "3",
+            "^v^v^v^v^v" to "11"
+        )
+
+}
