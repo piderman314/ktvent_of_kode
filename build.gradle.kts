059aef3ee3e041dffb280fa337d8a003928cc243
Mark Arts <piderman+github@gmail.com>
2024-12-29 13:41:54 +0100

    Add spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("com.diffplug.spotless") version "7.0.0.BETA4"
 }
 
 group = "arts.mark"
@@ -15,4 +16,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        ktfmt("0.51").kotlinlangStyle().configure {
+            it.setMaxWidth(120)
+        }
+    }
 }
