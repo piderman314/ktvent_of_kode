925b38e6585d2bb1f4298de069a1c04caa6002f7
Mark Arts <piderman+github@gmail.com>
2024-12-30 11:30:34 +0100

    Use kotlin regex

--- src/main/kotlin/advent/_2015/Day6.kt
+++ src/main/kotlin/advent/_2015/Day6.kt
@@ -4,7 +4,6 @@ import advent.Day
 import advent.util.Grid
 import advent.util.Location
 import java.time.LocalDate
-import java.util.regex.Pattern
 import kotlin.math.max
 
 const val TURN_ON = "turn on"
@@ -13,18 +12,17 @@ const val TOGGLE = "toggle"
 
 class Day6 : Day(LocalDate.of(2015, 12, 6)) {
 
-    val pattern: Pattern = Pattern.compile("($TURN_ON|$TURN_OFF|$TOGGLE) (\\d+),(\\d+) through (\\d+),(\\d+)")
+    private val regex: Regex = "($TURN_ON|$TURN_OFF|$TOGGLE) (\\d+),(\\d+) through (\\d+),(\\d+)".toRegex()
 
     override fun part1(input: String): String {
         val grid: Grid<Int> = Grid(1000, 1000, 0)
 
         input.lines().forEach { line ->
-            val matcher = pattern.matcher(line)
-            matcher.find()
-            val from = Location(matcher.group(2).toInt(), matcher.group(3).toInt())
-            val to = Location(matcher.group(4).toInt(), matcher.group(5).toInt())
+            val found = regex.find(line)!!
+            val from = Location(found.groups[2]!!.value.toInt(), found.groups[3]!!.value.toInt())
+            val to = Location(found.groups[4]!!.value.toInt(), found.groups[5]!!.value.toInt())
 
-            when (matcher.group(1)) {
+            when (found.groups[1]!!.value) {
                 TURN_ON -> grid[from..to] = 1
                 TURN_OFF -> grid[from..to] = 0
                 TOGGLE -> grid.replace(from..to) { it xor 1 }
@@ -38,12 +36,11 @@ class Day6 : Day(LocalDate.of(2015, 12, 6)) {
         val grid: Grid<Int> = Grid(1000, 1000, 0)
 
         input.lines().forEach { line ->
-            val matcher = pattern.matcher(line)
-            matcher.find()
-            val from = Location(matcher.group(2).toInt(), matcher.group(3).toInt())
-            val to = Location(matcher.group(4).toInt(), matcher.group(5).toInt())
+            val found = regex.find(line)!!
+            val from = Location(found.groups[2]!!.value.toInt(), found.groups[3]!!.value.toInt())
+            val to = Location(found.groups[4]!!.value.toInt(), found.groups[5]!!.value.toInt())
 
-            when (matcher.group(1)) {
+            when (found.groups[1]!!.value) {
                 TURN_ON -> grid.replace(from..to) { it + 1 }
                 TURN_OFF -> grid.replace(from..to) { max(0, it - 1) }
                 TOGGLE -> grid.replace(from..to) { it + 2 }
